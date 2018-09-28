d9c47a709a031f3916c5317d1f599a14d417d055
Maxime Portaz <maxime.portaz@gmail.com>
2018-09-28 10:24:45 +0200

    add unit test

---
+++ test_database.cpp
@@ -0,0 +1,13 @@
+#include "database/database.h"
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+	cout << "Unit test of DataBase" << endl;
+	DataBase db {};
+	//TODO
+	return 0;
+
+}
---
+++ test_dataentry.cpp
@@ -0,0 +1,18 @@
+#include "database/dataentry.h"
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+	cout << "Unit test of DataEntry" << endl;
+	DataEntry dE {10u};
+	cout << "Entry of size 10: " << dE << endl;
+
+#ifdef with_cuda
+	cout << "Test CUDA loading" << endl;
+	DataEntryGPU d_dE {10u};
+#endif
+	return 0;
+
+}
