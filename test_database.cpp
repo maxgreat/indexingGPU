f00adf3dc9e0333c84fa50ff2e55eb52d64d09d7
Maxime Portaz <maxime.portaz@gmail.com>
2018-11-06 16:50:01 +0100

    add new tests

--- test_database.cpp
+++ test_database.cpp
@@ -6,8 +6,10 @@ using namespace std;
 
 int main(){
 	cout << "Unit test of DataBase" << endl;
-	DataBase db {};
-	//TODO
+	DataBase db;
+	db.createNEntries(10);
+	cout << "DataBase of :" << db.getNbEntries() << "entries";
+	cout << "of size " << db.getSize() << endl;
 	return 0;
 
 }
