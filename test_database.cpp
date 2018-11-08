3225670ab118705deb5c5c72f3773bffa29626c6
Maxime Portaz <maxime.portaz@gmail.com>
2018-11-08 15:27:22 +0100

    add new unit tests

--- test_database.cpp
+++ test_database.cpp
@@ -1,4 +1,5 @@
 #include "database/database.h"
+#include "ml/knn.h"
 #include <iostream>
 #include <cstdio>
 
@@ -8,8 +9,20 @@ int main(){
 	cout << "Unit test of DataBase" << endl;
 	DataBase db;
 	db.createNEntries(10);
-	cout << "DataBase of :" << db.getNbEntries() << "entries";
-	cout << "of size " << db.getSize() << endl;
+	cout << "DataBase of : " << db.getNbEntries() << " entries " << endl;
+	cout << "of size : " << db.getSize() << endl;
+
+	cout << "Print database :" << endl;
+	for(auto i=0; i < db.getNbEntries(); i++){
+		cout << db[i] << endl;
+	}
+
+
+	DataEntry nEntry = DataEntry(5);
+	cout << "The closest to :" << nEntry << endl;
+	DataEntry& E = naive_1nn(nEntry, db);
+	cout << "is : " << E << endl;
+
 	return 0;
 
 }
